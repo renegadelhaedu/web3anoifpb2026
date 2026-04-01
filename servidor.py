d28b812498e68705ea805697c53514c8a250f65d
renegadelhaedu <renegadelha.edu@gmail.com>
2026-04-01 12:15:07 -0300

    aula 0104

---
+++ servidor.py
@@ -0,0 +1,12 @@
+#importar o flask para o projeto
+from flask import *
+
+#instanciar o objeto do servidor web flask
+app = Flask(__name__)
+
+@app.route('/')
+def inicial():
+    return render_template('eu.html')
+
+#executar o servidor
+app.run(host='0.0.0.0' , port=5007)
